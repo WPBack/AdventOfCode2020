03440183c0e459567c0ff64aab072c4ba6204e9a
WPBack <daniel.a.back@telia.com>
2020-12-12 16:16:42 +0100

    Finished day 9

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -7,6 +7,7 @@ inputFile = open(os.path.join(__location__, 'input.txt'), 'r')
 input = [int(line) for line in inputFile.read().splitlines()]
 
 # PART 1: Find the first entry that does not follow the rule
+firstInvalid = 0
 for i in range(25, len(input)):
     valid = False
     for j in range(24):
@@ -16,5 +17,18 @@ for i in range(25, len(input)):
                 if input[i-25+j+k] == diff:
                     valid = True
     if not valid:
-        print('Part 1: ', input[i])
+        firstInvalid = input[i]
+        break
+
+print('Part 1: ', firstInvalid)
+
+# PART 2: find a range that sums up to the invalid number
+for i in range(len(input)):
+    sum = input[i]
+    j = i+1
+    while sum < firstInvalid and j < len(input):
+        sum += input[j]
+        j += 1
+    if sum == firstInvalid:
+        print('Part 2: ', min(input[i:j]) + max(input[i:j]))
         break
