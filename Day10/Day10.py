4221c20bdf1df51e408ffad860a2ab9c62f5b9ba
WPBack <daniel.a.back@telia.com>
2020-12-12 17:43:03 +0100

    Solved day 10

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -1,6 +1,7 @@
 # Import modules
 import os
 import numpy as np
+from math import comb
 
 # Read input
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -18,3 +19,22 @@ input.insert(0,0)
 diff = np.diff(input).tolist()
 
 print('Part 1: ', diff.count(1)*diff.count(3))
+
+# PART 2: Count how many ways the joltage can be achieved
+print(input)
+print(diff)
+numCombinations = 1
+numOnesInOrder = 0
+for i in range(len(diff)):
+    if diff[i] == 1:
+        numOnesInOrder += 1
+    else:
+        if numOnesInOrder > 1:
+            currNumCombs = 0
+            for j in range(numOnesInOrder):
+                if not (j == 0 and numOnesInOrder > 3):
+                    currNumCombs += comb(numOnesInOrder-1, j)
+            numCombinations *= currNumCombs
+        numOnesInOrder = 0
+
+print('Part 2: ', numCombinations)
