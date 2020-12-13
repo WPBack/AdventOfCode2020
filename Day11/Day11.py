ef3682d3a29b14ef145bab73cdcd62fadcb0747b
WPBack <daniel.a.back@telia.com>
2020-12-13 10:33:27 +0100

    Solved day 11 part 2

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -7,8 +7,8 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 inputFile = open(os.path.join(__location__, 'input.txt'), 'r') 
 input = [list(line) for line in inputFile.read().splitlines()]
 
-# Function to update the map
-def updateMap(map):
+# PART 1: Function to update the map
+def updateMapPart1(map):
     changed = False
     newMap = copy.deepcopy(map)
     for row in range(len(map)):
@@ -38,11 +38,59 @@ def updateMap(map):
 map = copy.deepcopy(input)
 changed = True
 while changed:
-    (changed, map) = updateMap(map)
+    (changed, map) = updateMapPart1(map)
 
-# PART 1: Count the number of occuiped seats
+# Count the number of occuiped seats
 numOccupied = 0
 for row in map:
     numOccupied += row.count('#')
 
 print('Part 1: ', numOccupied)
+
+# PART 2: Function to check the first seat in a given direction
+def firstSeat(map, row, col, drow, dcol):
+    checkRow = row + drow
+    checkCol = col + dcol
+    while checkRow >= 0 and checkRow < len(map) and checkCol >= 0 and checkCol < len(map[0]):
+        if not map[checkRow][checkCol] == '.':
+            return map[checkRow][checkCol]
+        else:
+            checkRow += drow
+            checkCol += dcol
+    
+    return '.'
+
+# Function to count number of occupied first seats
+def numOccupiedFirst(map, row, col):
+    return [firstSeat(map, row, col, 1, 0), firstSeat(map, row, col, 1, 1), firstSeat(map, row, col, 0, 1), firstSeat(map, row, col, -1, 1), firstSeat(map, row, col, -1, 0), firstSeat(map, row, col, -1, -1), firstSeat(map, row, col, 0, -1), firstSeat(map, row, col, 1, -1)].count('#')
+    
+
+# Function to update the map
+def updateMapPart2(map):
+    changed = False
+    newMap = copy.deepcopy(map)
+    for row in range(len(map)):
+        for col in range(len(map[0])):
+            if map[row][col] == 'L':
+                if numOccupiedFirst(map, row, col) == 0:
+                    newMap[row][col] = '#'
+                    changed = True
+
+            elif map[row][col] == '#':
+                if numOccupiedFirst(map, row, col) >= 5:
+                    newMap[row][col] = 'L'
+                    changed = True
+    return (changed, newMap)
+
+# Update the map until it gets full
+map = copy.deepcopy(input)
+changed = True
+while changed:
+    (changed, map) = updateMapPart2(map)
+
+# Count the number of occuiped seats
+numOccupied = 0
+for row in map:
+    numOccupied += row.count('#')
+
+print('Part 2: ', numOccupied)
