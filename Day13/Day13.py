6b3fad2bba91bcf770470e7ac1c9622216a8e9fa
WPBack <daniel.a.back@telia.com>
2020-12-13 12:54:58 +0100

    First of day 13 part 2. Probably works, slow

--- Day13/Day13.py
+++ Day13/Day13.py
@@ -11,12 +11,21 @@ input = inputFile.read().splitlines()
 time = int(input[0])
 busLines = [int(bus) for bus in re.findall('\d+', input[1])]
 
-# Calculate the waiting time for each bus line
+# PART 1: Calculate the waiting time for each bus line
 waitTimes = [bus - time%bus for bus in busLines]
 
 # Find the minimum wait time and corresponding line
 minWaitTime = min(waitTimes)
 minWaitBus = busLines[waitTimes.index(minWaitTime)]
 
-# PART 1: Multiply the wait time with the bus line
 print('Part 1: ', minWaitTime*minWaitBus)
+
+# PART 2: Extract indices for the bus lines
+busLinesWithX = input[1].split(',')
+busLineIndices = [busLinesWithX.index(str(bus)) for bus in busLines]
+
+time = 0
+while not all([(busLines[i] - time%busLines[i]) == busLineIndices[i] for i in range(1,len(busLines))]):
+    time += busLines[0]
+
+print('Part 2: ', time)
