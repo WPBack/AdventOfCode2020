1a18f3002fca7981fb5fb0f077bd3a3361a83f0a
WPBack <daniel.a.back@telia.com>
2020-12-20 10:57:25 +0100

    Solved day 15 part 2

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -33,3 +33,19 @@ for i in range(len(input)+1, 2021):
     spoken[lastSpoken] = i
 
 print('Part 1: ', lastSpoken)
+
+# Part 2: Keep on going until the 30000000th number is spoken
+
+for i in range(2021, 30000001):
+    if firstTime:
+        lastSpoken = 0
+    else:
+        lastSpoken = i-1 - lastTurnSpoken
+
+    firstTime = not (lastSpoken in spoken)
+    if not firstTime:
+        lastTurnSpoken = spoken[lastSpoken]
+
+    spoken[lastSpoken] = i
+
+print('Part 2: ', lastSpoken)
