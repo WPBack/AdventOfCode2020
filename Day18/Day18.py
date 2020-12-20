ca87ab3e7f1021413a74800abb88683dfb901b30
WPBack <daniel.a.back@telia.com>
2020-12-20 19:56:00 +0100

    Started day 18 part 1, not finished

---
+++ Day18/Day18.py
@@ -0,0 +1,24 @@
+# Import modules
+import os
+import regex
+
+# Read input
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+inputFile = open(os.path.join(__location__, 'input.txt'), 'r') 
+input = inputFile.read().splitlines()
+
+# Recursive function to evaluate an expression
+def evalExp(exp):
+    parts = regex.finditer('(\+)|(\*)|(\d)|\((([^\)\(]+)|(?R))*+\)', exp)
+    operands = []
+    operators = []
+
+    for part in parts:
+        if part.group(0) == '+':
+            operators.append(part.group(0))
+        elif part.group(0) == '*':
+            operators.append(part.group(0))
+        elif part.group(0)[0] == '(':
+            operands.append(part.group(0)[1:-1])
+        else:
+            operands.append(part.group(0))
