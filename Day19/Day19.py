c2d602b387d7a7a27059c3b0426bc0142f844955
WPBack <daniel.a.back@telia.com>
2020-12-23 17:16:09 +0100

    Solved day 19 part 2

--- Day19/Day19.py
+++ Day19/Day19.py
@@ -45,3 +45,33 @@ for message in input[1].splitlines():
             numFullMatch += 1
 
 print('Part 1: ', numFullMatch)
+
+# Build rule 42 and 31
+rule42str = buildRule(42, rules)
+rule42re = re.compile(rule42str)
+rule31str = buildRule(31, rules)
+rule31re = re.compile(rule31str)
+
+# Check how many messages fully match the new rule 0
+numFullMatch = 0
+
+for message in input[1].splitlines():
+    numMatch42 = 0
+    numMatch31 = 0
+    start = 0
+    match42 = rule42re.match(message[start:])
+    while match42:
+        numMatch42 += 1
+        start += len(match42.group(0))
+        match42 = rule42re.match(message[start:])
+
+    match31 = rule31re.match(message[start:])
+    while match31:
+        numMatch31 += 1
+        start += len(match31.group(0))
+        match31 = rule31re.match(message[start:])
+
+    if start == len(message) and numMatch42 > numMatch31 and numMatch31 > 0:
+        numFullMatch += 1
+
+print('Part 2: ', numFullMatch)
